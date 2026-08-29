45c98c88db1797cb3c9326594f37ec05b0e28448
Rony Armenta <ronyarmenta38@gmail.com>
2026-08-29 10:18:05 -0700

    totalized function 1.1

--- CashRegister.js
+++ CashRegister.js
@@ -12,6 +12,8 @@ class cashRegister {
         this.unitTotal = 0;
         this.unitPrice = 0;
         this.total = 0;
+        this.tax = 0;
+        this.subtotal = 0;
 
     }
 
@@ -23,11 +25,37 @@ class cashRegister {
             console.log("Item " + itemName + " added to the cart. Price: $" + foundItem.price + ", Brand: " + foundItem.brand);
         } else {
             console.log("Item " + itemName + " not found");
-            
+         
         }
+    }
+
+    removeItemFromCart(itemName) {
+        const carItems = this.shoppingCart.find(item => item.name === itemName);
+        if (carItems) {
+            this.shoppingCart.pop(carItems);
+            console.log("Item: " + itemName + " have been removed from the cart");
+
+        }else{
+            console.log("Item: " + itemName + " does not exist in the cart");   
+        }
+
+    }
+
+    calculateTotal(){
+        let total = 0;
+        let taxes = 0;
+        let subtotal = 0;
+        this.shoppingCart.forEach(purshasedItem => {totalPriceAmount += item.price});
+        taxes = totalPriceAmount * 0.16;
+        subtotal = totalPriceAmount + taxes;
+        console.log("Sub total = " + subtotal);
+        console.log("Taxes = " + taxes);
+        console.log("Total = " + totalPriceAmount);
     }
 
 }
 
 const myShops = new cashRegister();
-myShops.addItemToCart("garrr"); 
+//myShops.addItemToCart("iPhone");
+myShops.calculateTotal();
+//myShops.removeItemFromCart("SmartTV"); 
